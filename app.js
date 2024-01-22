7394d0b8b979f83bbdfe44d80656c20697e09f76
ShuBox <64628603+shumasod@users.noreply.github.com>
2024-01-22 21:44:03 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+resources/js/app.js
+import Vue from 'vue';
+
+const app = new Vue({
+    el: '#app',
+});
+
+app.component('gohan-ok', {
+    template: `
+        <div>
+            <h1>ごはんですよ！</h1>
+        </div>
+    `,
+    mounted() {
+        this.sayGohanOk();
+    },
+    methods: {
+        sayGohanOk() {
+            alert('ごはんですよ！');
+        }
+    }
+});
