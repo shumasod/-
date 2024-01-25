2d4cd89c7cac80c4d79a474184032c407b22ed76
ShuBox <64628603+shumasod@users.noreply.github.com>
2024-01-25 22:47:08 +0900

    Create welcome.blade.php

---
+++ hack/welcome.blade.php
@@ -0,0 +1,20 @@
+resources/views/welcome.blade.php
+<html>
+<head>
+    <title>このPCハッキングされたよ！</title>
+
+    <style>
+        body {
+            background-color: #f0f0f0;
+        }
+
+        h1 {
+            font-size: 3em;
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <h1>このPCハッキングされたよ！</h1>
+</body>
+</html>
