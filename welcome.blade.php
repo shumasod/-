df44f581171f0f9208c744345973ee7fbe105f1a
ShuBox <64628603+shumasod@users.noreply.github.com>
2024-05-10 21:53:35 +0900

    Update welcome.blade.php

--- welcome.blade.php
+++ welcome.blade.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>ごはんよ！</title>
+  <title>ごはんdeathよ！</title>
+  
   <!-- Bootstrap CSS CDN -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
